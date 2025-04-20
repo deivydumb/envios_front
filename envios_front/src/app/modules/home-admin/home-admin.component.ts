3d2da983d0daa4b529ffb1b7555bbcf7926c6a9c
Deivyd Umbariba Gaitan <deivydumbaribag@live.com>
2025-04-19 23:41:40 -0500

    Creation Components Package, records and reports

--- envios_front/src/app/modules/home-admin/home-admin.component.ts
+++ envios_front/src/app/modules/home-admin/home-admin.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from "../home/components/header/header.component";
 import { Router } from '@angular/router';
 import { JourneyComponent } from "../journey/journey.component";
 import { NgIf } from '@angular/common';
+import { PackageAssigmentComponent } from "../package-assigment/package-assigment.component";
+import { ReportsComponent } from "../reports/reports.component";
 
 @Component({
   selector: 'app-home-admin',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent, JourneyComponent, NgIf],
+  imports: [FooterComponent, HeaderComponent, JourneyComponent, NgIf, PackageAssigmentComponent, ReportsComponent],
   templateUrl: './home-admin.component.html',
   styleUrl: './home-admin.component.css'
 })
@@ -19,13 +21,22 @@ export class HomeAdminComponent {
 
   constructor(private router: Router) {}
   username: string = 'Administrador';
+
   showjourney = false;
+  showPackage = false
+  showReports = false
 
   navegationForJourney(){
     this.showjourney = true;
   }
+  navegationForPackage(){
+    this.showPackage = true
+  }
   navigateTo(): void {
     this.showjourney = true;
+  }
+  navigateToReports(): void {
+    this.showReports = true;
   }
 
 
---
+++ envios_front/src/app/modules/package-assigment/package-assigment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-package-assigment',
+  standalone: true,
+  imports: [],
+  templateUrl: './package-assigment.component.html',
+  styleUrl: './package-assigment.component.css'
+})
+export class PackageAssigmentComponent {
+
+}
